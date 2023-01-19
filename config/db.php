dc357599ff85e78b2dec53093477b2237f79d15d
Dmytro <baristin43@gmail.com>
2023-01-19 18:44:38 +0200

    Created dbarannik branch

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_db',
     'username' => 'root',
-    'password' => '',
+    'password' => 'qqq123',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
