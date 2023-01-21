115fbca615ccbc0e2948d22ed8a9107fb687c8e4
Dmytro <baristin43@gmail.com>
2023-01-21 20:16:54 +0200

    feat(site/about): About page (updated content).

--- views/site/about.php
+++ views/site/about.php
@@ -9,7 +9,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="row">
 			<div class="col-12">
-				<div class="title-bordered mb-5 d-flex align-items-center">
+				<!-- <div class="title-bordered mb-5 d-flex align-items-center">
 					<h1 class="h4">Hi, I Am John Doe</h1>
 					<ul class="list-inline social-icons ml-auto mr-3 d-none d-sm-block">
 						<li class="list-inline-item"><a href="#"><i class="ti-facebook"></i></a>
@@ -21,14 +21,14 @@ $this->params['breadcrumbs'][] = $this->title;
 						<li class="list-inline-item"><a href="#"><i class="ti-github"></i></a>
 						</li>
 					</ul>
-				</div>
+				</div> -->
 				<!-- <img src="images/author-full.jpg" class="img-fluid w-100 mb-4 rounded-lg" alt="author"> -->
-                <img src="images/about_banner.jpg" style="max-width: 60%;" class="img-fluid w-100 mb-4 rounded-lg" alt="author">
+                <img src="images/about_banner.png" style="max-width: 100%;" class="img-fluid w-100 mb-4 rounded-lg" alt="author">
 				<div class="content">
 					<p>Відомо, що cучасний перioд розвитку цивiлiзованого суспільства характеризує прoцеc iнфoрматизацiї. Інформацiйнi технологiї - це oдин iз cучасних cпоcобiв cпiлкування, головними перевагами якого є загальнoдoступнicть. Індустрія інформаційних технологій в Україні набирає все більших обертів.</p>
-    <p><b>IT BLOG</b> - одна з лідируючих платформ, що дозволяє ділитись та розповсюджувати найсвіжіші новини з усього світу. Кожен день наша команда робить усе можливе, щоб надати можливість обговорювати та ділитись новинами в сфері ІТ. Саме тут ви можете знайти статті стосовно технологій, які є трендом у всьому світі.</p>
-    <p>Тож реєструйся зараз і насолоджуйся!</p>
-					<p>Faucibus duis urna, consequat morbi imperdiet pulvinar. Mauris vitae sed aliquet eu tincidunt id. Nulla leo eget erat vel pharetra. Morbi nullam morbi non purus, augue. Massa cras odio ante cursus a mauris. Volutpat faucibus feugiat feugiat euismod congue ridiculus. Et ipsum euismod nulla et aenean diam duis. Sem molestie est mauris sit. Et eleifend odio</p>
+                    <p><b>IT BLOG</b> - одна з лідируючих платформ, що дозволяє ділитись та розповсюджувати найсвіжіші новини з усього світу. Кожен день наша команда робить усе можливе, щоб надати можливість обговорювати та ділитись новинами в сфері ІТ. Саме тут ви можете знайти статті стосовно технологій, які є трендом у всьому світі.</p>
+                    <p>Тож реєструйся зараз і насолоджуйся!</p>
+					<!-- <p>Faucibus duis urna, consequat morbi imperdiet pulvinar. Mauris vitae sed aliquet eu tincidunt id. Nulla leo eget erat vel pharetra. Morbi nullam morbi non purus, augue. Massa cras odio ante cursus a mauris. Volutpat faucibus feugiat feugiat euismod congue ridiculus. Et ipsum euismod nulla et aenean diam duis. Sem molestie est mauris sit. Et eleifend odio</p>
 					<div class="quote"> <i class="ti-quote-left"></i>
 						<div>
 							<p>Life is a series of natural and spontaneous changes. Don’t resist them – that only create sorrow. Let reality changes be reality. Let things flow naturally way they like.</p> <span class="quote-by"> -John Doe</span>
@@ -43,6 +43,6 @@ $this->params['breadcrumbs'][] = $this->title;
 						<li>Donec vitae sapien utlorem</li>
 					</ul>
 					<p>Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna.</p>
-				</div>
+				</div> -->
 			</div>
 		</div>
