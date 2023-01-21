98459f001b4c952051bc728313a1ae238002cc09
Dmytro <baristin43@gmail.com>
2023-01-21 14:11:20 +0200

    feat(site/about): banner for About page.

--- views/site/about.php
+++ views/site/about.php
@@ -8,8 +8,12 @@ $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
-
+    <!-- <h1><?= Html::encode($this->title) ?></h1> -->
+    <!-- <img src="images/about_banner.png" alt="альтернативний текст"> -->
+    <div style="text-align: center;">
+        <img src="images/about_banner.png" style="max-width: 60%;" class="img-fluid" alt="...">
+    </div>
+    
     <p><b>IT BLOG</b> - одна з лідируючих платформ, що дозволяє ділитись та розповсюджувати найсвіжіші новини з усього світу. Кожен день наша команда робить усе можливе, щоб надати можливість обговорювати та ділитись новинами в сфері ІТ. Саме тут ви можете знайти статті стосовно технологій, які є трендом у всьому світі.</p>
     <p>Тож реєструйся зараз і насолоджуйся!</p>
 
